0e351ee5134b61745db840beeed8c68e11803a65
ttomczak3 <78294042+ttomczak3@users.noreply.github.com>
2021-11-03 19:57:31 -0500

    Save slider values

---
+++ LugwigJam/Assets/Scripts/SaveSFX.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveSFX : MonoBehaviour
+{
+
+    public Slider sliderSFX;
+
+    public float sliderValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sliderSFX.value = PlayerPrefs.GetFloat("save", sliderValue);   
+    }
+
+    public void ChangeSlider(float value)
+    {
+        sliderValue = value;
+        PlayerPrefs.SetFloat("save", sliderValue);
+    }
+
+}
